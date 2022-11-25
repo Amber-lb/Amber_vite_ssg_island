3547fe8358acc2b0f173c65f42587e6a48f8fe42
Amber-lb <359564897@qq.com>
2022-11-25 23:56:40 +0800

    预览island

---
+++ docs/.island/dist/assets/astro.7aece713.js
@@ -0,0 +1,4 @@
+import{jsx as e,jsxs as o,Fragment as a}from"react/jsx-runtime";const s=void 0,i=[],d="Astro";function r(t){const n=Object.assign({h1:"h1",a:"a",p:"p"},t.components);return o(a,{children:[o(n.h1,{id:"astro",children:[e(n.a,{className:"header-anchor","aria-hidden":"true",href:"#astro",children:"#"}),"Astro"]}),`
+`,e(n.p,{children:"\u6D4B\u8BD5\u6548\u679C"})]})}function h(t={}){const{wrapper:n}=t.components||{};return n?e(n,Object.assign({},t,{children:e(r,t)})):r(t)}const l="Invalid Date",m=`# Astro\r
+\r
+\u6D4B\u8BD5\u6548\u679C`;export{m as content,h as default,s as frontmatter,l as lastUpdatedTime,d as title,i as toc};
